fccae74685ea78f158330a6560daa8825c404587
haiguanghu <Lionhu2008>
2020-09-15 10:52:41 +0900

    add avatar for user model

--- exrate2020/authentication/admin.py
+++ exrate2020/authentication/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
-from .models import User
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-admin.site.register(User)
+from .models import User
+
+
+
+class AccountAdmin(BaseUserAdmin):
+    list_display = ('email', 'username',  'is_staff',  'is_superuser')
+    list_filter = ('is_superuser',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'is_staff', 'is_superuser', 'password')}),
+        ('Personal info', {'fields': ('username', 'avatar')}),
+        ('Groups', {'fields': ('groups',)}),
+        ('Permissions', {'fields': ('user_permissions',)}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'is_staff', 'is_superuser', 'password1', 'password2')}),
+        ('Personal info', {'fields': ('username', 'avatar')}),
+        ('Groups', {'fields': ('groups',)}),
+        ('Permissions', {'fields': ('user_permissions',)}),
+    )
+
+    search_fields = ('email', 'name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, AccountAdmin)
