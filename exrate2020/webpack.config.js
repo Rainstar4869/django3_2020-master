7562950dadf4f7521c52beee77ee403f50ce39b6
haiguanghu <Lionhu2008>
2020-09-17 20:23:27 +0900

    integrated Vue to Django - #2
    webpack integrated successfully

--- exrate2020/webpack.config.js
+++ exrate2020/webpack.config.js
@@ -3,8 +3,9 @@ var webpack = require('webpack');
 var VueLoaderPlugin = require('vue-loader/lib/plugin');
 var BundleTracker = require('webpack-bundle-tracker');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // installed via npm
 var ManifestPlugin = require('webpack-manifest-plugin');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
@@ -13,6 +14,10 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 // const smp = new SpeedMeasurePlugin();
 var env = process.env.NODE_ENV;
 
+const CopyPatterns=[
+           { from: './vueapp/vue_django/src/assets',to:"./assets"}]
+
+
 module.exports ={
   entry:{
     main:['babel-polyfill', './vueapp/vue_django/src/main.js'],
@@ -20,10 +25,7 @@ module.exports ={
   devtool: '#eval-source-map',
   output: {
     filename: "[name].js",
-    // filename: "[name]-[hash].js",
-    // chunkFilename: '[name].bundle.js',
     path: path.resolve('./staticfiles/bundles/'),
-    // publicPath: 'http://localhost:8080/static/',
   },
   watch: true,
   watchOptions: {
@@ -31,75 +33,96 @@ module.exports ={
     poll: 1000,
     ignored: /node_modules/
   },
-  module: {
+    module: {
         rules: [
-	        {
-    			    test: /\.vue$/,
-    			    loader: 'vue-loader',
-    			    options: {
-    			        loaders: {
-    			            'scss': [
-    			                'vue-style-loader',
-    			                'css-loader',
-    			                'sass-loader'
-    			            ],
-    			            'sass': [
-    			                'vue-style-loader',
-    			                'css-loader',
-    			                'sass-loader?indentedSyntax'
-    			            ]
-    			        }
-    			    }
-    			},
-	        {
-    			    test: /\.js$/,
-    			    loader: 'babel-loader',
-    			    exclude: /node_modules/,
-              options:{
-                plugins:['syntax-dynamic-import']
-              },
-    			},
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        'scss': [
+                            'vue-style-loader',
+                            'css-loader',
+                            'sass-loader'
+                        ],
+                        'sass': [
+                            'vue-style-loader',
+                            'css-loader',
+                            'sass-loader?indentedSyntax'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /.(js|jsx)$/,
+                include: [path.resolve(__dirname, 'src/js')],
+                loader: 'babel-loader'
+            },
+            {
+                test: /.js$/,
+                include: [path.resolve(__dirname, 'src/js')],
+                loader: 'eslint-loader'
+            },
+            {
+                test: /.css$/,
+                // include: [path.resolve(__dirname, 'src/styles')],
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                                 options: {
+              publicPath: '../'
+            }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.sass$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader?indentedSyntax'
+                ],
+            },
+            {
+                test: /\.(otf|eot|svg|ttf|woff|woff2)(\?.+)?$/,
+                loader: 'url-loader'
+            },
+            {
+        test: /.(gif|png|jpg)$/,
+        include: [path.resolve(__dirname, 'src/images')],
+        use: [
           {
-              test: /\.css$/,
-              use: [
-                  'vue-style-loader',
-                  'css-loader'
-              ],
-          },
-          {
-              test: /\.scss$/,
-              use: [
-                  'vue-style-loader',
-                  'css-loader',
-                  'sass-loader'
-              ],
-          },
-          {
-              test: /\.sass$/,
-              use: [
-                  'vue-style-loader',
-                  'css-loader',
-                  'sass-loader?indentedSyntax'
-              ],
-          },
-          {
-            test: /\.(otf|eot|svg|ttf|woff|woff2)(\?.+)?$/,
-            loader: 'url-loader'
-          },
-         {
-           test: /\.(png|svg|jpg|gif)$/,
-           use: [
-             'file-loader',
-           ],
-         },
+            loader: 'url-loader',
+            // ↓ 追加
+            options: {
+              limit: 30720,
+              name: '[name].[ext]',
+              outputPath: './images/'
+            }
+            // ↑ 追加
+          }
+        ]
+      },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
         ]
     },
   plugins:[
+      new webpack.ProgressPlugin(),
   	new VueLoaderPlugin(),
     new ManifestPlugin(),
-    // new BundleAnalyzerPlugin({
-    //   analyzerPort:18888
-    // }),
+      new CopyWebpackPlugin({patterns:CopyPatterns}),
+    new MiniCssExtractPlugin({ filename: 'css/style.[chunkhash].css' }),
   	new BundleTracker({filename: './webpack-stats.json'}),
     new CleanWebpackPlugin(),
 
@@ -115,9 +138,22 @@ module.exports ={
         }
   },
   optimization: {
-        splitChunks: {
+        // splitChunks: {
+        //   name: 'vendor',
+        //   chunks: 'initial',
+        //  }
+          minimizer: [new TerserPlugin()],
+      splitChunks: {
+          cacheGroups: {
+              vendors: {
+                  priority: -10,
+                  test: /[\\/]node_modules[\\/]/
+              }
+          },
+          minChunks: 1,
+          minSize: 30000,
           name: 'vendor',
           chunks: 'initial',
-         }
+      }
      }
 };
