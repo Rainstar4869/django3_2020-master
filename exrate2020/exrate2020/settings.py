349709254fd49198de698489ba29a9f62920b70e
haiguanghu <Lionhu2008>
2020-10-09 11:10:08 +0900

    debug intcomma problem

--- exrate2020/exrate2020/settings.py
+++ exrate2020/exrate2020/settings.py
@@ -243,12 +243,10 @@ CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 LANGUAGE_CODE = 'ja'
 
 TIME_ZONE = 'Asia/Tokyo'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = False
+USE_THOUSAND_SEPARATOR = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
