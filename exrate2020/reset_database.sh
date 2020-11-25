926ce776c246613ade279d940fcf1f882eaa2a7e
haiguanghu <Lionhu2008>
2020-11-25 15:50:54 +0900

    add gitignore to system pgdata temp folder

--- exrate2020/reset_database.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
--- exrate2020/web_auth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebAuthConfig(AppConfig):
-    name = 'web_auth'
